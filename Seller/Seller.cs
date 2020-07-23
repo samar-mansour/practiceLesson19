21fce5ca784b178bad178497a51f01e7c32989e6
samar-mansour <52625367+samar-mansour@users.noreply.github.com>
2020-07-23 16:11:49 +0300

    Add files via upload

---
+++ Seller/Seller.cs
@@ -0,0 +1,25 @@
+﻿using PracticeLesson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seller
+{
+    public class Seller
+    {
+        private int moneyEarrend;
+
+        public void SellNewspaper(Newspaper newspaper)
+        {
+            this.moneyEarrend = Convert.ToInt32(this.moneyEarrend + newspaper.GetPrice());
+            Console.WriteLine($"Money Earned: {this.moneyEarrend}");
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} Money earned: {this.moneyEarrend}";
+        }
+    }
+}
