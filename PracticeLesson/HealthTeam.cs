664f4e2005db35f0778987f9ad0e6b47aac24532
samar-mansour <52625367+samar-mansour@users.noreply.github.com>
2020-08-07 16:05:32 +0300

    Update HealthTeam.cs

--- PracticeLesson/HealthTeam.cs
+++ PracticeLesson/HealthTeam.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,14 +13,14 @@ namespace PracticeLesson
             string text = Console.ReadLine();
             Console.BackgroundColor = ConsoleColor.Green;
             Console.WriteLine(story.GetVisualStyle());
-            Console.WriteLine(story.SetVisualStyle(text));
+            story.SetVisualStyle(text);
         }
 
         internal override void CreateStory()
         {
-            Console.WriteLine(story.GetTitle());
-            Console.WriteLine(story.GetBody());
-            Console.WriteLine(story.GetVisualStyle());
+            story.SetTitle(story.GetTitle());
+            story.SetBody(story.GetBody());
+            story.SetVisualStyle(story.GetVisualStyle());
         }
     }
 }
